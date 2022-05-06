a75ad5f219198d46f91a4d2f18e72d6451174ff4
Anton Popov <ademaro@ya.ru>
2022-05-06 21:23:43 +0530

    Добавлена функция

--- script0.py
+++ script0.py
@@ -3,6 +3,19 @@ def calc(x, y):
     return x + y
 
 
-result = calc(55, 23)
+# result = calc(55, 23)
+# result2 = calc(552, 231)
 
-print(result)
+# print(result)
+# print(result2)
+
+def check_my_number(my_var_param, number_to_check):
+    if my_var_param > number_to_check:
+        print(f"{my_var_param} больше {number_to_check}")
+    elif my_var_param < number_to_check:
+        print(f"{my_var_param} меньше {number_to_check}")
+    else:
+        print(f"{my_var_param} равно {number_to_check}")
+
+my_var = 151
+check_my_number(my_var, 15)
