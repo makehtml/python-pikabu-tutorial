b1f5158584344df96b02f9afa1bee159a3d8011f
Lemilien <105110542+Lemilien@users.noreply.github.com>
2022-05-09 18:36:42 +0300

    Добавлен файл Spisokdel.py

---
+++ Spisokdel.py
@@ -0,0 +1,21 @@
+# Создаем словарь
+ds = {}
+
+
+Data = input("Введите дату:")
+Plans = input("Введите задачу:")
+# Ключ дата значение планы
+ds[Data] = Plans
+
+Data = input("Введите дату:")
+Plans = input("Введите задачу:")
+ds[Data] = Plans
+
+Data = input("Введите дату:")
+Plans = input("Введите задачу:")
+ds[Data] = Plans
+
+
+print(Data, Plans)
+# Все запрашиваемые данные будут направлены в словарь
+print(ds)
