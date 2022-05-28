6e22c4841cca4b837e944489f88a9e282a136711
Anton Popov <ademaro@ya.ru>
2022-05-28 17:19:58 +0530

    Добавлен контроллер ядра

---
+++ app/modules/core/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+core = Blueprint("core", __name__, template_folder="templates")
+
+
+@core.route("/index")
+@core.route("/")
+def index():
+    return 'OK'  # render_template("pages/index.j2")
