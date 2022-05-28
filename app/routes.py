789c16a4084e29380cc3c4e56c298cbc943372b0
Anton Popov <ademaro@ya.ru>
2022-05-28 17:12:43 +0530

    Добавлен базовый скелет flask-приложения

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+bps = [
+    {"module": "core", "view": "views", "name": "core"},
+    # {"module": "core", "view": "admin_views", "name": "admin", "prefix": "/admin"},
+    # {"module": "core", "view": "errors", "name": "error"},
+    # {"module": "pages", "view": "views", "name": "page"},
+    # {"module": "pages", "view": "admin_views", "name": "admin_page", "prefix": "/admin/pages"},
+    # {"module": "api", "view": "views", "name": "api", "prefix": "/api"},
+]
