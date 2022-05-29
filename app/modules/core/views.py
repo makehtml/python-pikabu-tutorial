cac7dc414d9e645a8a0cec1587e1531e371667f5
frcjocker <frcjocker@gmail.com>
2022-05-29 22:41:30 +0800

    Сделаны исправления в базовом шаблоне

--- app/modules/core/views.py
+++ app/modules/core/views.py
@@ -6,4 +6,4 @@ core = Blueprint("core", __name__, template_folder="templates")
 @core.route("/index")
 @core.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.j2")
