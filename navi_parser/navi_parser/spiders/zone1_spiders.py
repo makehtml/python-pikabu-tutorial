900486a0102888f3f403c97600f0cbc72679292a
Nima <nimaamogolonov@mail.ru>
2022-05-30 18:45:47 +0300

    Попытка запарсить через css.

--- navi_parser/navi_parser/spiders/zone1_spiders.py
+++ navi_parser/navi_parser/spiders/zone1_spiders.py
@@ -7,15 +7,16 @@ class Zone1Spider(scrapy.Spider):
     def start_requests(self):
         urls = [
             # 'https://armada.defensa.gob.es/ihm/Aplicaciones/Navareas/Index_Navareas_xml_en.html',
-            'https://www.admiralty.co.uk/maritime-safety-information/radio-navigational-warnings/low-bandwidth/navarea-1'
+         'https://www.admiralty.co.uk/maritime-safety-information/radio-navigational-warnings/low-bandwidth/navarea-1'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        id = response.xpath('/html/body/form/div[4]/div/div/div/main/div/span/article/div/div[2]/div[2]/div[1]/div/div/div/div[1]/table/tbody/tr[1]/td[2]/text()')
-        date = response.xpath('//*[@id="ctl00_ctl39_g_7a56dbd8_1bd3_49a5_8f17_204f53697688"]/table/tbody/tr[1]/td[3]/text()')
-        area = response.xpath('//*[@id="fa3ed36a-fd5c-4d47-b282-6cb1ed8bb870"]/td[3]/div/div[1]/text()')
-        title = response.xpath('//*[@id="fa3ed36a-fd5c-4d47-b282-6cb1ed8bb870"]/td[3]/div/div[2]/text()')
-        description = response.xpath('//*[@id="fa3ed36a-fd5c-4d47-b282-6cb1ed8bb870"]/td[3]/div/div[4]/text(), //*[@id="fa3ed36a-fd5c-4d47-b282-6cb1ed8bb870"]/td[3]/div/div[5]/text()')
-
+        yield{
+            'id': response.css('td.optional-column::text').get(),
+            'date': response.css('h3.accordion-content-cell::text').get(),
+            'area': response.css('div.ExternalClassE94DAD22C5514EC681C0E7A85F785CFB::text').get()[0],
+            'title': response.css('div.ExternalClassE94DAD22C5514EC681C0E7A85F785CFB::text').get()[1],
+            'description': response.css('div.ExternalClassE94DAD22C5514EC681C0E7A85F785CFB::text').get()[3][4]
+        }
