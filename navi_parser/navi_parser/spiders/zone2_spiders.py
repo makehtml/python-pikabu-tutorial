9b5b92535742b1c7e16dea69c2c84852a6e930b3
Anton Popov <ademaro@ya.ru>
2022-05-31 22:31:30 +0530

    Удалён лишний файл

--- navi_parser/navi_parser/spiders/zone2_spiders.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-url = 'https://gan.shom.fr/navarea/NavareaIIenVigueur.txt'
-params = {'title': 'navarea', 'id'= 'id', 'date': 'date', 'area':'area', 'description': 'description'}
-response = requests.get(url,params = params)
-response.json
